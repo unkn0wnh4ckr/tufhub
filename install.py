c5e759586ed3760ece742a52b505cee488f10ed5
un_kn0wn h4c_k3r <40415158+unkn0wnh4ckr@users.noreply.github.com>
2018-11-22 03:00:39 -0500

    Add files via upload

---
+++ install.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+def install():
+	os.system("apt update")
+	os.system("pip install mechanize json whois python-whois requests bs4 requests[socks] urlparse cookielib") 
+	os.system("pip install scapy datetime argparse re threading urllib2 modules")
+	os.system("apt install python-socks")
+	os.system("apt install metasploit-framework")
+	os.system("apt install setoolkit")
+	os.system("apt install wifite")
+	os.system("apt install reaver")
+	os.system("apt install aircrack-ng")
+	os.system("apt install nmap")
+	os.system("apt install php")
+	os.system("apt install perl")
+
+
+
+
+
+print "are you running on the real kali linux os   [y/n]"
+check = raw_input("[y/n]> ")
+if check == "y" :
+	print "good"
+	os.system("sleep 2")
+	install()
+if check == "n" :
+	print "then a decent amount of tools in this script might not work"
+	print "do you want to continue installation  [y/n]"
+	install = raw_input("[y/n]> ")
+	if install == "y" :
+		install()
+	if install == "n" :
+		print "thanks for checking out my script"
+		sys.exit
