2829a7c75e6c25299e9e00c5302c49a6c03ec610
un_kn0wn h4c_k3r <40415158+unkn0wnh4ckr@users.noreply.github.com>
2018-11-23 14:15:23 -0500

    Update install.py

--- install.py
+++ install.py
@@ -33,6 +33,23 @@ if check == "n" :
 	print "do you want to continue installation  [y/n]"
 	install = raw_input("[y/n]> ")
 	if install == "y" :
+		def install():
+			os.system("apt update")
+			os.system("pip install mechanize json whois python-whois requests bs4 requests[socks] urlparse cookielib") 
+			os.system("pip install scapy datetime argparse re threading urllib2 modules builtwith smtplib")
+			os.system("apt install python-socks")
+			os.system("apt install nmap")
+			os.system("apt install php")
+			os.system("apt install perl")
+			print """entering big download region prepare you anus
+			if your not ready press ctrl C"""
+			os.system("sleep 5")
+			os.system("apt install metasploit-framework")
+			os.system("apt install setoolkit")
+			os.system("apt install wifite")
+			os.system("apt install reaver")
+			os.system("apt install aircrack-ng")
+
 		install()
 	if install == "n" :
 		print "thanks for checking out my script"
