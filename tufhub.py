1f0a94751f8c5975f6b9a3211133347e05cf2f35
un_kn0wn h4c_k3r <40415158+unkn0wnh4ckr@users.noreply.github.com>
2018-11-24 01:00:27 -0500

    Update tufhub.py

--- tufhub.py
+++ tufhub.py
@@ -50,9 +50,8 @@ O = '\033[33m'
 p = '\033[35m'
 os.system("service tor start")
 os.system("clear")
-print "i did not make most of the tools in here so dont think im saying that"
+print "i did not most of the tools in here so dont think im saying that"
 os.system("sleep 3")
-os.system("clear")
 def striker():
   params = []
   # Browser
@@ -1100,6 +1099,219 @@ def exploits():
     os.system("clear")
     mainbanner()
 
+def billcipher():
+  def back():
+      print()
+      back = raw_input('\033[92mDo you want to continue? [Yes/No]: ')
+      if back[0].upper() == 'Y':
+          print()
+          iseeverything()
+      elif back[0].upper() == 'N':
+          print('\033[92mRemember https://GitHackTools.blogspot.com')
+          exit
+      else:
+          print('\033[92m?')
+          exit
+
+  def clear():
+    os.system('clear')
+
+  def bill():
+      clear()
+      print("""\033[93m ######                   #####                                
+   #     # # #      #      #     # # #####  #    # ###### #####  
+   #     # # #      #      #       # #    # #    # #      #    # 
+   ######  # #      #      #       # #    # ###### #####  #    # 
+   #     # # #      #      #       # #####  #    # #      #####  
+   #     # # #      #      #     # # #      #    # #      #   #  
+   ######  # ###### ######  #####  # #      #    # ###### #    # 2.3
+   Information Gathering tool for a Website or IP address""")
+      print()
+
+  def banner():
+      print("""\033[96m
+   1) DNS Lookup                 13) Host DNS Finder
+   2) Whois Lookup               14) Reserve IP Lookup
+   3) GeoIP Lookup               15) Email Gathering (use Infoga)
+   4) Subnet Lookup              16) Subdomain listing (use Sublist3r)
+   5) Port Scanner               17) Find Admin login site (use Breacher)
+   6) Page Links                 18) Check and Bypass CloudFlare (use HatCloud)
+   7) Zone Transfer              19) Website Copier (use httrack)
+   8) HTTP Header                20) Host Info Scanner (use WhatWeb)
+   9) Host Finder                21) About BillCipher
+   10) IP-Locator                22) Fuck Out Of Here (Exit)
+   11) Find Shared DNS Servers
+   12) Get Robots.txt""")
+      print()
+
+  def iseeverything():
+      try:
+          what = raw_input('\033[92mAre you want to collect information of website or IP address? [W/I]: ')
+          if what[0].upper() == 'W':
+              victim = raw_input('Enter the website address: ')
+              banner()
+          elif what[0].upper() == 'I':
+              victim = raw_input('Enter the IP address (or domain to get IP address of this domain): ')
+              victim = socket.gethostbyname(victim)
+              print('The IP address of target is:',victim)
+              banner()
+          else:
+              print('?')
+              iseeverything()
+
+          choose = raw_input('What information would you like to collect? (1-20): ')
+
+          if choose == '1':
+              dnslook = 'https://api.hackertarget.com/dnslookup/?q='+victim
+              info = requests.get(dnslook)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '2':
+              whois = 'https://api.hackertarget.com/whois/?q='+victim
+              info = requests.get(whois)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '3':
+              ipgeo = 'https://api.hackertarget.com/geoip/?q='+victim
+              info = requests.get(ipgeo)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '4':
+              subnet = 'http://api.hackertarget.com/subnetcalc/?q='+victim
+              info = requests.get(subnet)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '5':
+              port = 'https://api.hackertarget.com/nmap/?q='+victim
+              info = requests.get(port)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '6':
+              pagelink = 'https://api.hackertarget.com/pagelinks/?q='+victim
+              info = requests.get(pagelink)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '7':
+              zone = 'https://api.hackertarget.com/zonetransfer/?q='+victim
+              info = requests.get(zone)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '8':
+              header = 'https://api.hackertarget.com/httpheaders/?q='+victim
+              info = requests.get(header)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '9':
+              host = 'https://api.hackertarget.com/hostsearch/?q='+victim
+              info = requests.get(host)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '10':
+              iplt = 'https://ipinfo.io/'+victim+'/json'
+              info = requests.get(iplt)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '11':
+              shared = 'https://api.hackertarget.com/findshareddns/?q='+victim
+              info = requests.get(shared)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '12':
+              robots ='http://'+victim+'/robots.txt'
+              info = requests.get(robots)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '13':
+              hostdns = 'https://api.hackertarget.com/mtr/?q='+victim
+              info = requests.get(hostdns)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '14':
+              reserve = 'https://api.hackertarget.com/reverseiplookup/?q='+victim
+              info = requests.get(reserve)
+              print('\033[91m',info.text)
+              back()
+
+          elif choose == '15':
+              clear()
+              os.system('cd modules/Infoga && python3 infoga.py --domain '+victim)
+              back()
+
+          elif choose == '16':
+              clear()
+              os.system('cd modules/Sublist3r && python3 sublist3r.py -d '+victim)
+              back()
+
+          elif choose == '17':
+              clear()
+              os.system('cd modules/Breacher && python breacher.py -u '+victim)
+              back()
+
+          elif choose == '18':
+              clear()
+              os.system('ruby ./modules/HatCloud/hatcloud.rb -b '+victim)
+              back()
+
+          elif choose == '19':
+              os.system('cd websource && mkdir '+victim)
+              os.system('cd websource && cd '+victim+' && httrack '+victim)
+              print("The website source code was saved in folder 'websource'")
+              back()
+
+          elif choose == '20':
+              clear()
+              os.system('whatweb -v '+victim)
+              back()
+
+          elif choose == '21':
+              print("""\033[93mBillCipher 2.1 - Information Gathering of a Website or IP address
+
+  AUTHOR: https://GitHackTools.blogspot.com
+          https://twitter.com/SecureGF#
+          https://fb.com/TVT618
+          https://plus.google.com/+TVT618""")
+              back()
+
+          elif choose == '22':
+              exit
+
+          else:
+              print('?')
+              iseeverything()
+              
+      except socket.gaierror:
+          print('Name or service not known!\033[93m')
+          print()
+          iseeverything()
+      except UnboundLocalError:
+          print('The information you entered is incorrect')
+          print()
+          iseeverything()
+      except requests.exceptions.ConnectionError:
+          print('Your Internet Offline')
+          exit
+      except IndexError:
+          print('?')
+          print()
+          iseeverything()
+
+  bill()
+  iseeverything()
+
+
 def infobanner():
   print B+'''
            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄                 
@@ -1136,6 +1348,7 @@ def info():
   print "[4]  quick ip grab"
   print "[5]  portscan only"
   print "[6]  nmap"
+  print "[7]  BillCipher"
   print G+"-" * 100
   info = raw_input(B+"[INFO] \033[92m->: \033[0m")
   if info == "1" :
@@ -1157,6 +1370,8 @@ def info():
     print "Type nmap commands   Example: --> nmap www.pornhub.com -b -F"
     nmap = raw_input(R+"--> \033[0m")
     os.system(nmap)
+  if info == "7" :
+    billcipher()
   if info == "exit" :
     print Y+"Exiting..."
     os.system("sleep 2")
